3c27f0ba91a1850c53e0304016dea2070ae3dde2
Baur Pauline <paulinebaur02@gmail.com>
2020-07-08 10:23:59 +0200

    push 2

--- EX_01/mini-site-routing.php
+++ EX_01/mini-site-routing.php
@@ -13,8 +13,26 @@
         <li><a href="mini-site-routing.php?page=2">Page 1</a></li><br/>
         <li><a href="mini-site-routing.php?page=3">Page 2</a></li><br/>
         <li><a href="connexion.php?page=connexion">Connexion</a></li><br/>
-        <li><a href="admin.php?page=admin">Admin</a></li><br/>
     </nav>
+
+    <?php
+    if($_COOKIE['id'])
+    {
+        echo '<a href="admin.php?page=admin">Admin</a>';
+    }
+    ?>
+
+    <form enctype="multipart/form-data" action="admin.php" method="post">
+        <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
+        <input name="userfile" type="file" accept="image/x-png,image/jpg,image/jpeg" />
+        <input name="descriptione" type="text" placeholder="Description" />
+        <input name="titre" type="text" placeholder="Titre" />
+        <input type="submit" value="Envoyer le fichier" />
+    </form>
+
+    <?php
+        echo $_FILES;
+    ?>
 
     <?php
     if($_GET)
@@ -35,12 +53,7 @@
     {
         echo "<h1>Connexion</h1>";
     }
-    if($_GET["page"]== "admin")
-    {
-        echo "<h1>Admin</h1>";
     }
-    }
-   
 
     ?>
 
@@ -54,17 +67,11 @@ session_start();
 if(isset($_SESSION["id"]))
 {
     echo '<p>Login : ' .$_SESSION["id"]. '</p>';
+    echo '<p> Description : ' .$_SESSION["description"]. '</p>';
 }
-
-
-
-if ($_COOKIE['id'])
+else
 {
-    echo $_COOKIE['id'];
-}
-else 
-{
-    echo "<p><a href='http://localhost/ISCC/ISCC-2020-J09/EX_01/connexion.php?page=connexion'>Revenir au formulaire.</a></p>";
+    header('http://localhost/ISCC/ISCC-2020-J09/EX_01/connexion.php?page=connexion');
 }
 ?>
 </footer>
